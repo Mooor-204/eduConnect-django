f793322acc51cc1c58f030ea2418f6120e221df4
maryam <M.hesham2395@nu.edu.eg>
2025-11-06 20:57:44 +0300

    feat(admin): add Faculty model and admin dashboard
    - Create Faculty model with university data fields
    - Register Faculty model with Django admin
    - Add migration for database schema
    - Enable admin to manage universities and faculties

--- universities/admin.py
+++ universities/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Faculty
 
-# Register your models here.
+@admin.register(Faculty)
+class FacultyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'university', 'fees', 'required_percent']
+    list_filter = ['university', 'education_system']
--- universities/models.py
+++ universities/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-from django.db import models
-from django.contrib.auth.models import User
-
 class Faculty(models.Model):
     EDUCATION_SYSTEMS = [
         ('american', 'American'),
@@ -10,29 +7,13 @@ class Faculty(models.Model):
         ('national', 'National'),
     ]
     
-    NATIONAL_CATEGORIES = [
-        ('literature', 'Literature'),
-        ('scientific_science', 'Scientific Science'),
-        ('scientific_mathematics', 'Scientific Mathematics'),
-    ]
-    
     name = models.CharField(max_length=200)
     university = models.CharField(max_length=200)
     fees = models.IntegerField()
-    credits = models.IntegerField()
+    credits = models.IntegerField(default=120)
     required_percent = models.FloatField()
     extra_requirements = models.TextField()
-    education_system = models.CharField(max_length=20, choices=EDUCATION_SYSTEMS)
-    national_category = models.CharField(max_length=25, choices=NATIONAL_CATEGORIES, blank=True, null=True)
+    education_system = models.CharField(max_length=20, choices=EDUCATION_SYSTEMS, default='national')
     
     def __str__(self):
         return f"{self.name} - {self.university}"
-
-class StudentProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    high_school_percent = models.FloatField(null=True, blank=True)
-    education_system = models.CharField(max_length=20, choices=Faculty.EDUCATION_SYSTEMS)
-    national_category = models.CharField(max_length=25, choices=Faculty.NATIONAL_CATEGORIES, blank=True, null=True)
-    
-    def __str__(self):
-        return f"{self.user.username} - {self.high_school_percent}%"
