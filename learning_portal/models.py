70571fde93be26fa20df722be10d429b367a5c58
SalmaAssem25 <s.sherif2371@nu.edu.eg>
2025-12-15 00:31:56 +0200

    feat: create learning_portal app with Course model

---
+++ learning_portal/models.py
@@ -0,0 +1,23 @@
+# learning_portal/models.py
+from django.db import models
+
+class Course(models.Model):
+    MAJOR_CHOICES = [
+        ('CS', 'Computer Science'),
+        ('ENG', 'Engineering'),
+        ('MED', 'Medicine'),
+        ('BUS', 'Business'),
+        ('ART', 'Arts & Humanities'),
+        ('SCI', 'Natural Sciences'),
+    ]
+    
+    title = models.CharField(max_length=200)
+    major = models.CharField(max_length=50, choices=MAJOR_CHOICES)
+    provider = models.CharField(max_length=100)
+    url = models.URLField()
+    duration_hours = models.IntegerField()
+    free = models.BooleanField(default=True)
+    description = models.TextField()
+    
+    def __str__(self):
+        return f"{self.title} ({self.get_major_display()})"
