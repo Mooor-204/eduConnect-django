86645c44678615793c00c83537b07071e1bb0f1f
hussienbadawy19 <h.hany2304@nu.edu.eg>
2025-12-20 03:46:35 +0200

    Add test cases for auth_app

--- auth_app/tests.py
+++ auth_app/tests.py
@@ -1,3 +1,62 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class AuthAppTests(TestCase):
+    def setUp(self):
+       
+        self.username = "testuser"
+        self.password = "testpass123"
+        self.user = User.objects.create_user(username=self.username, password=self.password)
+        self.client = Client()
+
+    def test_signup_view_get(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'auth_app/signup.html')
+
+    def test_signup_view_post_success(self):
+        response = self.client.post(reverse('signup'), {
+            'username': 'newuser',
+            'password1': 'newpassword123',
+            'password2': 'newpassword123'
+        })
+        self.assertEqual(response.status_code, 302)  
+        self.assertTrue(User.objects.filter(username='newuser').exists())
+
+    def test_signup_view_post_failure(self):
+        response = self.client.post(reverse('signup'), {
+            'username': '', 
+            'password1': 'password123',
+            'password2': 'password123'
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'This field is required')
+
+    def test_login_view_get(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'auth_app/login.html')
+
+    def test_login_view_post_success(self):
+        response = self.client.post(reverse('login'), {
+            'username': self.username,
+            'password': self.password
+        })
+        self.assertEqual(response.status_code, 302)  
+        self.assertTrue('_auth_user_id' in self.client.session)
+
+    def test_login_view_post_failure(self):
+        response = self.client.post(reverse('login'), {
+            'username': self.username,
+            'password': 'wrongpass'
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Invalid username or password.')
+
+    def test_logout_view(self):
+        
+        self.client.login(username=self.username, password=self.password)
+        response = self.client.get(reverse('logout'))
+        self.assertEqual(response.status_code, 302) 
+        self.assertFalse('_auth_user_id' in self.client.session)
